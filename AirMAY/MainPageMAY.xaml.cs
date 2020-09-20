0cedc3ed4e5818819cef462cf77e92f36b48f0bf
My code FOSSA <alina.cherednichenko.2000@gmail.com>
2020-09-20 19:13:11 +0300

    List binding

--- AirMAY/MainPageMAY.xaml.cs
+++ AirMAY/MainPageMAY.xaml.cs
@@ -1,4 +1,5 @@
-﻿using AirMAY.Services;
+﻿using AirMAY.Domain.Models;
+using AirMAY.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -28,7 +29,49 @@ namespace AirMAY
 
         private async void AviaButton_Click(object sender, RoutedEventArgs e)
         {
-            mainListBox.ItemsSource = await _flightService.GetAllFlight();
+            var res = await _flightService.GetAllFlight();
+            List<Flight> flights = new List<Flight>();
+
+            foreach (var item in res)
+            {
+                foreach (var times in item.FlightTimes)
+                {
+                    flights.Add(new Flight()
+                    {
+                        Price = item.Price,
+                        FirstSity = item.FirstSity,
+                        SecondSity = item.SecondSity,
+                        FlightTimes = new List<FlightTime>() { times }
+                    });
+                }
+            }
+            mainListBox.ItemsSource = flights;
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var res = await _flightService.GetAllFlight();
+            List<Flight> flights = new List<Flight>();
+
+            foreach (var item in res)
+            {
+                foreach (var times in item.FlightTimes)
+                {
+                    flights.Add(new Flight()
+                    {
+                        Price = item.Price,
+                        FirstSity = item.FirstSity,
+                        SecondSity = item.SecondSity,
+                        FlightTimes =new List<FlightTime>() { times }
+                    });
+                }
+            }
+            mainListBox.ItemsSource = flights;
+        }
+
+        private void HistoryClick(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
